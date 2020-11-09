3fb7ed03a5249d85d80ecb81f4213e4664ffba4f
luba.t <luba@t.me>
2020-11-09 22:06:56 +0200

    init react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello Vetal
+      </div>
+    );
+  }
+}
