68dd5bce5b0f8651e40407f01a3f5e7e7197fffb
luba.t <lubo4ka.boiko@gmail.com>
2020-11-10 22:15:08 +0200

    remove blank line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 export default class App extends React.Component {
   render() {
-    
     return (
       <div>
         <h1>Amazing todos app</h1>
