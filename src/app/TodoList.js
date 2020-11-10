92a58e7aebefcbaf1626b4c42637137f4b005cf4
luba.t <lubo4ka.boiko@gmail.com>
2020-11-10 22:48:53 +0200

    component example

--- src/app/TodoList.js
+++ src/app/TodoList.js
@@ -2,16 +2,28 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  add() {
+    console.log('add')
+  }
+
   render() {
     return (
       <div>
         <div>
           <label for='new'>Enter what to do</label>
           <input type='text' className='task' id='todo'/>
-          <button id='add'> ADD </button>
+          <button id='add' onClick={ ()=>{ this.add() } }> ADD </button>
         </div>
         <div id='list'>
           <h1>Your todos</h1>
+          { JSON.stringify(this.state.items) }
         </div>
       </div>
     );
