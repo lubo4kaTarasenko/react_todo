a0201ce448763c7c80a408123598aead3c29f555
luba.t <lubo4ka.boiko@gmail.com>
2020-11-11 15:16:51 +0200

    api get all items

--- src/app/TodoList.js
+++ src/app/TodoList.js
@@ -5,49 +5,63 @@ export default class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: null,
+      isLoaded: false,
       items: []
     }
   }
-
-  add() {
-    this.update()
+  componentDidMount() {
+    fetch("http://localhost:3001/api/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
-  read() {
-    let todo = document.getElementById('todo').value   
-    return todo
+  renderList() {
+    const { error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <div>Помилка: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Завантаження...</div>
+    } else {
+      return (
+        <ul id='todolist'>
+          {items.map(item => (
+            <li key={item.id}>
+              <input value={item.text} className={ `todoitem ${item.color}` }/>
+              <input type="checkbox" checked={item.check} />
+            </li>
+          ))}
+        </ul>
+      )
+    };
   }
-
-  update() {
-    this.state.items.push(this.read())
-    this.setState({
-      items: this.state.items
-    })   
-  }
-
-
 
   render() {
     return (
       <div>
-        <div>
-          <label for='new'>Enter what to do</label>
-          <input type='text' className='task' id='todo'/>
-          <button id='add' onClick={ ()=>{ this.add() } }> ADD </button>
-        </div>
-        <div id='list'>
-          <h1>Your todos</h1>
-          <ul>
-          {
-            this.state.items.map((e)=>{
-              return (
-                <li>{e}</li>
-              )
-            })
-          }
-          </ul>
-        </div>
-      </div>
-    );
-  }
+          <div>
+            <label for='new'>Enter what to do</label>
+            <input type='text' className='task' id='todo'/>
+            <button id='add' /*onClick={ ()=>{ this.add() } }*/> ADD </button>
+          </div>
+          <div id='list'>
+            <h1>Your todos</h1>
+            { this.renderList() }
+          </div>
+      </div>  
+    )}  
 }
